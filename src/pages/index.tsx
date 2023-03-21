5f25d817d0b2b650693ef143e05b720429a31d48
Muhammed Shittu <devshittu@gmail.com>
2023-03-21 16:53:55 +0000

    Tailwind set up and prettierignore and eslintignore added

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -16,6 +16,8 @@ export default function Home() {
       </Head>
       <main className={styles.main}>
         <div className={styles.description}>
+          <h1 className="text-8xl font-bold underline">Hello world!</h1>
+
           <p>
             Get started by editing&nbsp;
             <code className={styles.code}>src/pages/index.tsx</code>
