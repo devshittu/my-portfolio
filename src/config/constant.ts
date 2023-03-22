614a35df639b24b6446d1323670edf0a245b098f
Muhammed Shittu <devshittu@gmail.com>
2023-03-22 16:19:06 +0000

    Designed the softwares and tools.

---
+++ src/config/constant.ts
@@ -0,0 +1,16 @@
+export const API_URL = process.env.NEXT_PUBLIC_API_URL as string;
+
+export const LINKEDIN_URL =
+  process.env.NEXT_MY_LINKEDIN_URL || 'https://linkedin.com/in/muhammedshittu';
+export const MY_CONTACT_EMAIL = process.env.NEXT_MY_CONTACT_EMAIL || 'mshittu.work@gmail.com';
+export const MY_CALENDLY_URL =
+  process.env.NEXT_MY_CALENDLY_URL || 'https://calendly.com/mshittuchat/meetme';
+
+export const TOOLS_AND_SOFTWARES = [
+  'React',
+  'TypeScript',
+  'Next.js',
+  'Figma',
+  'Storybook',
+  'Node.js',
+];
