1c6765256cc316d6520aa1fe61c4e81b1e7ab15e
Muhammed Shittu <devshittu@gmail.com>
2023-03-24 22:51:32 +0000

    Applied link in the landing page.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,8 +5,7 @@ import styles from '@/styles/Home.module.css';
 import { TOOLS_AND_SOFTWARES } from '@/config/constant';
 import { ReactElement } from 'react';
 import { PublicLayout } from '@/layouts/public-layout';
-
-const inter = Inter({ subsets: ['latin'] });
+import { Link } from '@/components/link';
 
 const Home = function () {
   return (
@@ -38,20 +37,28 @@ const Home = function () {
                   sections for your landing page.{' '}
                 </p>
                 <div className="flex flex-col justify-center lg:flex-row ">
-                  <a
-                    target="_blank"
-                    className="flex items-center   font-normal rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700"
-                  >
-                    <svg
-                      className="flex-shrink-0 w-8 h-8 transition duration-75 dark:text-gray-400 "
-                      fill="currentColor"
-                      viewBox="0 0 20 20"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <path d="M5 3a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2V5a2 2 0 00-2-2H5zM5 11a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2v-2a2 2 0 00-2-2H5zM11 5a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2V5zM11 13a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2v-2z"></path>
-                    </svg>
-                    <span className="flex-1 ml-3 whitespace-nowrap">LinkedIn</span>
-                  </a>
+                  <Link
+                    href="#"
+                    text="LinkedIn"
+                    icon={
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="24"
+                        height="24"
+                        viewBox="0 0 24 24"
+                        fill="none"
+                        stroke="currentColor"
+                        strokeWidth="2"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        className="flex-shrink-0 w-6 h-6 transition duration-75 dark:text-gray-400 "
+                      >
+                        <path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z" />
+                        <rect x="2" y="9" width="4" height="12" />
+                        <circle cx="4" cy="4" r="2" />
+                      </svg>
+                    }
+                  />
                 </div>
               </div>
               <div className="w-full lg:w-1/3 lg:max-w-lg md:w-1/2 hidden md:block">
@@ -87,8 +94,8 @@ const Home = function () {
                   An engineer with a strong design background, specializing in design systems,
                   accessibility and interface design for digital products
                 </p>{' '}
-                <div className="">LinkedIn Resume</div>
-                <div className="flex flex-col flex-wrap lg:flex-row mt-6 lg:mb-0 mb-6 w-10/12 gap-4">
+                {/* <div className="">LinkedIn Resume</div> */}
+                <div className="flex flex-wrap flex-row mt-6 lg:mb-0 mb-6 w-10/12 gap-4">
                   <a
                     target="_blank"
                     href="https://linkedin.com/in/muhammedshittu"
