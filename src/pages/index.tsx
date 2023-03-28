26d00a0e0c81ca49380a09e4acb52d8c05607d26
Muhammed Shittu <devshittu@gmail.com>
2023-03-28 15:45:57 +0100

    Applied project items card widget in the home page.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,6 +7,7 @@ import { ReactElement } from 'react';
 import { PublicLayout } from '@/layouts/public-layout';
 import { Link } from '@/components/link';
 import profilePhoto from '../../public/me.jpeg';
+import ProjectItems from '@/components/widgets/project-cards/project-items';
 
 const Home = function () {
   return (
@@ -183,45 +184,7 @@ const Home = function () {
               </p>
             </div>
             <div className="section-body mt-12">
-              <ul className="list-none flex flex-col lg:flex-row flex-wrap md:w-10/12 mt-4 -mx-4">
-                <li className="hover:bg-slate-200 dark:hover:bg-slate-800 rounded">
-                  <div className="md:flex p-4  overflow-hidden">
-                    <div className="md:flex-shrink-0">
-                      <Image
-                        className="rounded-lg w-full md:w-56"
-                        src="https://dummyimage.com/224x170/F3F4F7/8693ac"
-                        alt="Woman paying for a purchase"
-                        width={224}
-                        height={170}
-                      />
-                    </div>
-                    <div className="mt-4 md:mt-0 md:ml-6">
-                      <div className="uppercase tracking-wide text-sm font-bold">
-                        <i className="fas fa-bullhorn mr-2"></i>Marketing
-                      </div>
-                      <a href="#" className="block mt-1 text-lg hover:underline font-bold">
-                        Looking for blogger
-                      </a>
-
-                      <p className="mt-2">
-                        Getting a new business off the ground is a lot of hard work. Here are five
-                        ideas you can use to find your first customers.
-                      </p>
-                      <div className="pt-4 pb-2">
-                        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
-                          #photography
-                        </span>
-                        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
-                          #travel
-                        </span>
-                        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
-                          #winter
-                        </span>
-                      </div>
-                    </div>
-                  </div>
-                </li>
-              </ul>
+              <ProjectItems />
             </div>
           </section>
           {/* Tools and Features */}
