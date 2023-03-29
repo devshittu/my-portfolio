c99e38a0b22e246d2e0848298fef995bbcbf1d27
Muhammed Shittu <devshittu@gmail.com>
2023-03-29 01:45:28 +0100

    Renamed old projectcards to articles cards .

---
+++ src/components/widgets/article-cards/article-items.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import ArticleItem from './article-item';
+import WeatherAppThumbnail from '../../../../public/project-weather-app.jpg';
+
+const ArticleItems = () => {
+  return (
+    <ul className="list-none flex flex-col lg:flex-row flex-wrap md:w-10/12 mt-4 -mx-4">
+      <ArticleItem
+        thumbnail={WeatherAppThumbnail}
+        title="Weather App"
+        description="Built with Vite, ReactJS, and Vanilla JS, this weather website automatically detects your location or nearby areas and gives you the latest weather updates. Plus, it features a sleek dark mode theme, so you can stay up to date on the weather no matter the time of day. 🌙"
+        githubUrl="https://github.com/devshittu/weather-app"
+      />
+    </ul>
+  );
+};
+
+export default ArticleItems;
