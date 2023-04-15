fca82c0f321078ba323a74a3cf753e5178be5360
Muhammed Shittu <devshittu@gmail.com>
2023-04-15 18:30:30 +0100

    Configured the axios

--- src/libs/api-client.ts
+++ src/libs/api-client.ts
@@ -1,1 +1,20 @@
+import { API_URL } from './../config/constant';
 import axios from 'axios';
+
+import { HASHNODE_API_URL } from '@/config/constant';
+
+export const apiClient = axios.create({
+  baseURL: HASHNODE_API_URL,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+apiClient.interceptors.response.use(
+  (response) => response.data,
+  (error) => {
+    const message = error.response?.data?.message || error.message;
+    console.error(message);
+    return Promise.reject(error);
+  },
+);
