ae4525eaad9f64b4465cd68ee40ddc567e85500c
Muhammed Shittu <devshittu@gmail.com>
2023-04-15 21:53:36 +0100

    Modified the app providers to handle Error gracefully.

--- src/providers/app.tsx
+++ src/providers/app.tsx
@@ -1,9 +1,10 @@
 import useOnline from '@/libs/hooks/use-online';
 import { ThemeProvider } from 'next-themes';
-import { queryClient } from '@/libs/react-query';
 import { QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { ReactNode } from 'react';
+import { ErrorBoundary } from 'react-error-boundary';
+import { queryClient } from '@/libs/react-query';
 
 type AppProviderProps = {
   children: ReactNode;
@@ -15,10 +16,13 @@ export const AppProvider = ({ children, theme }: AppProviderProps) => {
   return (
     <ThemeProvider attribute="class" forcedTheme={theme || undefined}>
       {/* {`You are ${online ? 'online' : 'offline'}`} */}
-      <QueryClientProvider client={queryClient}>
-        <ReactQueryDevtools initialIsOpen={false} />
-      </QueryClientProvider>
-      {children}
+
+      <ErrorBoundary fallback={<div>Something went wrong!</div>} onError={console.error}>
+        <QueryClientProvider client={queryClient}>
+          <ReactQueryDevtools initialIsOpen={false} />
+          {children}
+        </QueryClientProvider>
+      </ErrorBoundary>
     </ThemeProvider>
   );
 };
