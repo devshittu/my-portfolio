8b7447021ba4f963c61e87acda1debea1fa44cb8
Muhammed Shittu <devshittu@gmail.com>
2023-04-15 19:01:25 +0100

    impletemented get articles api layer

---
+++ src/features/articles/api/get-articles.ts
@@ -0,0 +1,28 @@
+import { apiClient } from '@/libs/api-client';
+import { Article } from '../types';
+import { useQuery } from '@tanstack/react-query';
+
+type GetArticlesOptions = {
+  params: {
+    postId: string | undefined;
+  };
+};
+
+export const getArticles = ({ params }: GetArticlesOptions): Promise<Article[]> => {
+  return apiClient.get('/articles', {
+    params,
+  });
+};
+
+export const useArticles = ({ params }: GetArticlesOptions) => {
+  const { data, isFetching, isFetched } = useQuery({
+    queryKey: ['Articles', params],
+    queryFn: () => getArticles({ params }),
+    enabled: !!params.postId,
+    initialData: [],
+  });
+  return {
+    data,
+    isLoading: isFetching && !isFetched,
+  };
+};
