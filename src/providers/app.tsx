4f95ab78182e0d55719290bfafd476adef67cc63
Muhammed Shittu <devshittu@gmail.com>
2023-04-15 18:45:57 +0100

    react-query setup in place

--- src/providers/app.tsx
+++ src/providers/app.tsx
@@ -1,6 +1,8 @@
-// 'use client';
 import useOnline from '@/libs/hooks/use-online';
 import { ThemeProvider } from 'next-themes';
+import { queryClient } from '@/libs/react-query';
+import { QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { ReactNode } from 'react';
 
 type AppProviderProps = {
@@ -13,6 +15,9 @@ export const AppProvider = ({ children, theme }: AppProviderProps) => {
   return (
     <ThemeProvider attribute="class" forcedTheme={theme || undefined}>
       {/* {`You are ${online ? 'online' : 'offline'}`} */}
+      <QueryClientProvider client={queryClient}>
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
       {children}
     </ThemeProvider>
   );
