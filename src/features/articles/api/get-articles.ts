62d18450c6333f8b346f229329d0aa30f0699d9a
Muhammed Shittu <devshittu@gmail.com>
2023-04-16 19:24:48 +0100

    Added api for feature articles.

--- src/features/articles/api/get-articles.ts
+++ src/features/articles/api/get-articles.ts
@@ -1,28 +1,59 @@
-import { apiClient } from '@/libs/api-client';
+import { gql } from 'graphql-request';
+import { apiClient, graphQLClient } from '@/libs/api-client';
 import { Article } from '../types';
 import { useQuery } from '@tanstack/react-query';
+import { MY_HASHNODE_USERNAME } from '@/config/constant';
 
 type GetArticlesOptions = {
-  params: {
-    postId: string | undefined;
+  params?: {
+    query?: string | undefined;
   };
+
+  limit?: number | undefined;
 };
 
-export const getArticles = ({ params }: GetArticlesOptions): Promise<Article[]> => {
-  return apiClient.get('/articles', {
-    params,
+export const getArticles = async (limit?: number): Promise<Article[]> => {
+  const queryQL = `
+    {
+      user(username: "${MY_HASHNODE_USERNAME}") {
+        publication {
+          posts(page: 0) {
+            _id
+            title
+            brief
+            coverImage
+            slug
+            dateAdded
+          }
+        }
+      }
+    }
+  `;
+  const getArticleList = await apiClient.get('/', {
+    params: {
+      query: queryQL,
+    },
   });
+  // return getArticleList.data.user.publication.posts;
+
+  let articles = getArticleList.data.user.publication.posts;
+  if (limit !== undefined && limit !== null) {
+    articles = articles.slice(0, limit); // only return the first 'limit' articles
+  }
+  return articles;
 };
 
-export const useArticles = ({ params }: GetArticlesOptions) => {
-  const { data, isFetching, isFetched } = useQuery({
-    queryKey: ['articles', params],
-    queryFn: () => getArticles({ params }),
-    enabled: !!params.postId,
+export const useArticles = (limit?: number) => {
+  const { data, isFetching, isFetched, error, isSuccess } = useQuery({
+    queryKey: ['articles'],
+    queryFn: () => getArticles(limit),
+    // enabled: !!params.postId,
     initialData: [],
   });
   return {
     data,
     isLoading: isFetching && !isFetched,
+    error,
+    isSuccess,
   };
 };
