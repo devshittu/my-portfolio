bc21278274fc17d50623e5f023f74d371cf351cf
Muhammed Shittu <devshittu@gmail.com>
2023-04-16 19:34:35 +0100

    Created a new page for articles still under construction

--- src/pages/articles/index.tsx
+++ src/pages/articles/index.tsx
@@ -1,18 +1,24 @@
 import { useArticles } from '@/features/articles';
+import { ArticlesList } from '@/features/articles/components/articles-list';
 import { PublicLayout } from '@/layouts/public-layout';
-import React from 'react';
+import React, { ReactNode } from 'react';
 
 const ArticlesPage = () => {
-  const articles = useArticles({
-    params: {
-      postId: '1' ?? '',
-    },
-  });
-  console.log(articles);
+  const { data, error, isLoading, isSuccess } = useArticles();
 
-  if (articles.isLoading) return <div>Loading</div>;
 
-  return <div>ArticlesPage</div>;
+  if (isLoading) return <div>Loading</div>;
+
+  return (
+    <>
+      <main className="py-10 md:py-24">
+        <article className="mx-auto max-w-2xl lg:max-w-4xl lg:px-12 text-justify">
+          {/* <div>ArticlesPage</div> */}
+          <ArticlesList articles={data} isLoading={isLoading} />
+        </article>
+      </main>
+    </>
+  );
 };
 
 ArticlesPage.getLayout = function getLayout(page: React.ReactElement) {
