30e11caf05ee9b11bdb639744c26f9d1aba895f0
Muhammed Shittu <devshittu@gmail.com>
2024-02-14 22:18:46 +0000

    Defined new types for the upgraded gql api

--- src/features/articles/types.ts
+++ src/features/articles/types.ts
@@ -6,3 +6,58 @@ export type Article = {
   slug: string;
   dateAdded: string;
 };
+
+// Define the types for the response data
+export type CoverImage = {
+  url: string | null;
+};
+
+export type Author = {
+  id: string;
+  username: string;
+  name: string;
+  profilePicture: string;
+  followersCount: number;
+  tagline: string;
+  bio: {
+    text: string;
+  };
+  location: string;
+};
+
+export type ArticleNode = {
+  title: string;
+  brief: string;
+  url: string;
+  id: string;
+  coverImage: CoverImage;
+  slug: string;
+  publishedAt: string;
+  author: Author;
+};
+
+export type Edge = {
+  node: ArticleNode;
+};
+
+export type Posts = {
+  edges: Edge[];
+};
+
+export type Publication = {
+  isTeam: boolean;
+  title: string;
+  posts: Posts;
+};
+
+export type GetArticlesResponse = {
+  publication: Publication;
+};
+
+export type GetArticlesOptions = {
+  params?: {
+    query?: string | undefined;
+  };
+
+  limit?: number | undefined;
+};
