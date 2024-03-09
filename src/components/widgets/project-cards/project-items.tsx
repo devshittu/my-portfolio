eeed7e72894715dff4b07668ad9c045de1d178dc
Muhammed Shittu <devshittu@gmail.com>
2024-03-09 20:05:13 +0000

    Added preview link

--- src/components/widgets/project-cards/project-items.tsx
+++ src/components/widgets/project-cards/project-items.tsx
@@ -10,6 +10,7 @@ export const ProjectItems = () => {
         title="Weather App"
         description="Built with Vite, ReactJS, and Vanilla JS, this weather website automatically detects your location or nearby areas and gives you the latest weather updates. Plus, it features a sleek dark mode theme, so you can stay up to date on the weather no matter the time of day. 🌙"
         githubUrl="https://github.com/devshittu/weather-app"
+        liveUrl="https://weather-app-devshittu.vercel.app/"
       />
     </div>
   );
